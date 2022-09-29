1556d81b47b474d4633e40d0c0dbeb4ff0867d10
amoreno6 <111070003+amoreno6@users.noreply.github.com>
2022-09-29 11:09:05 -0400

    Create FarmAnimal.java

---
+++ FarmAnimal.java
@@ -0,0 +1,16 @@
+public class FarmAnimal
+{
+  private String name;
+  private String gender;
+  private int weight;
+  private int age;
+  
+  public FarmAnimal(String name, String gender, int weight, int age)
+    {
+        this.name = name;
+        this.gender = gender;
+        this.weight = weight;
+        this.age = age;
+    }
+  
+  // getters and setters then toString
