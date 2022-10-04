96baa6c3037f86de81f032da891aaf640b20fa39
amoreno6 <111070003+amoreno6@users.noreply.github.com>
2022-10-04 10:33:52 -0400

    Create Flight.java

---
+++ Flight.java
@@ -0,0 +1,14 @@
+/** Class: Flight.java
+*@author Adrian Moreno
+* Course: ITEC 2150
+* Written: 10/4/2022
+*
+* This class – Interface class with fly method
+*//
+
+package Flight;
+
+public interface Flight 
+{
+    public void fly();
+}
