437fa5ab9ec1df594bb14c53dab4f48c1a5a4a3b
amoreno6 <111070003+amoreno6@users.noreply.github.com>
2022-10-04 17:11:56 -0400

    Create Movement.java

---
+++ Movement.java
@@ -0,0 +1,16 @@
+/** Class: Airplane.java
+*@author Adrian Moreno
+* Course: ITEC 2150
+* Written: 10/4/2022
+*
+* This class – Interface class extending to the flight class
+*//
+
+
+package Flight;
+
+public interface Movement extends Flight
+{
+    public void walk();
+    public void jump();
+}
